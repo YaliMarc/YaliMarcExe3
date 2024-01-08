829d2dbf337248a6b82f93f19bbc16e66d47aa4c
YaliMarc <yahali.marz@sisma.org.il>
2024-01-08 15:11:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/yalimarcexe3/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.yalimarcexe3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button bt;
+    int count=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt=findViewById(R.id.button);
+        bt.setText("Click Me");
+    }
+
+    public void exe(View view) {
+        count++;
+        String str="This is a click number:"+count;
+        bt.setText(str);
+        if (count==6)
+            bt.setText("Enough to click. Go to new start!")
+            count=0;
+);
+    }
+}
